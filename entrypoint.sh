75e50cf42c5c9bb2e8d79ea5bbb2210db52d4947
bonitoFotso <bonitofotso55@gmail.com>
2025-04-22 12:36:32 +0100

    ds

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ then
     echo "PostgreSQL démarré"
 fi
 
-# Si vous avez besoin de charger des données initiales gg
+# Si vous avez besoin de charger des données initiales gg d
 echo "Chargement des données initiales..."
 python manage.py seed_client
 python manage.py seed_docs
